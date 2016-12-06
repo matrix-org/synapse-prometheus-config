547eed08844eeec38dcc2c579168254bd198cf3e
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-12-06 14:57:33 +0000

    Bound the number of chunks stored in memory

--- start.sh
+++ start.sh
@@ -13,7 +13,11 @@ STORAGE_DAYS=60
 # Prometheus doesn't understand "days" as a unit
 STORAGE_HOURS=$(( $STORAGE_DAYS * 24 ))
 
+# Try to limit the amount of memory prometheus will use
+MEMORY_CHUNKS=524288
+
 nohup ../prometheus/prometheus -config.file=prometheus.yaml \
+    -storage.local.memory-chunks=${MEMORY_CHUNKS} \
     -storage.local.path=metrics \
     -storage.local.retention=${STORAGE_HOURS}h \
     -web.external-url=http://matrix.org/prometheus/ \
